9fe9419c8d7c0af2ad05e2964e860cb8f56936a7
Alan Sanders <alan@blockreg.io>
2023-12-02 08:53:09 -0800

    Updated Fetch
    Fetch now returns JSON data to the page when a button is clicked

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var el = document.getElementById('addEducation');
+
+function boomTrain(){
+    window.alert('Boom train');
+}
+
+async function getEducation(){
+    try {
+        const url = "./data/education.json";
+        const response = await fetch(url);
+        const data = await response.json();
+        var div = document.getElementById('education');
+        if (response.status == 200){
+            div.innerHTML = processEducation(data);
+            console.log(data)
+        }
+
+    } catch (err) {console.log(err)}
+    
+}
+
+function processEducation(education){
+    const educationlist = education.degrees.map(education => {
+        return `
+        <h2>
+        ${education.school}
+        </h2>
+        `
+    })
+
+    return `
+    ${educationlist.join('')}
+    `;
+}
+
+el.addEventListener('click', getEducation, false);
